90deef0028d6b1742b57363bf328aa150ed5ab36
Okellothomas <thomasolal33@gmail.com>
2022-04-09 14:38:09 +0300

    Add the quote form component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    showQuotes: boolean;
+    constructor(public name: string, public author: string, public quote: string, public published: Data){
+        this.showQuotes = false;
+    }
 }
