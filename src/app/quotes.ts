1706fc58f8d694ff76eb4dabbb5a33b7c8d893f3
Okellothomas <thomasolal33@gmail.com>
2022-04-09 15:06:50 +0300

    Add the quotes to the quote component and next in the app component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     showQuotes: boolean;
-    constructor(public name: string, public author: string, public quote: string, public published: Data){
+    constructor(public id:number, public name: string, public author: string, public quote: string, public published: Date){
         this.showQuotes = false;
     }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
+import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes: Quotes[] = [
+    new Quotes(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son','sureone',new Date()),
+    new Quotes(2, 'Watch finding Nemo', 'Find an online version and watch merlin find his son','sureone',new Date()),
+    new Quotes(3, 'Watch finding Nemo', 'Find an online version and watch merlin find his son','sureone',new Date()),
+    new Quotes(4, 'Watch finding Nemo', 'Find an online version and watch merlin find his son','sureone',new Date()),
+    new Quotes(5, 'Watch finding Nemo', 'Find an online version and watch merlin find his son','sureone',new Date())
+  ];
 
   constructor() { }
 
